c756bc4fc66f377320bcf3dbba2dbd2f13db4712
vimleshtech <35063663+vimleshtech@users.noreply.github.com>
2018-01-23 04:03:09 -0600

    python session 23rd jan

---
+++ Python 23rd Jan/prog1.py
@@ -0,0 +1,25 @@
+#a =1
+#print(type(a))
+
+
+'''
+a='1'
+print(type(a))
+
+a ='raman'
+print(type(a))
+
+a ="raman"
+print(type(a))
+
+a =True
+print(type(a))
+'''
+
+a = [1,2,3,43] # list 
+print(type(a))
+
+a =222.33
+print(type(a))
+
+
---
+++ Python 23rd Jan/prog2.py
@@ -0,0 +1,35 @@
+n1 = int(input('enter data :'))
+n2 = int(input('enter data :'))
+
+
+print(type(n1))
+print(type(n2))
+
+
+
+#n = int(n1) + int(n2)
+n = n1+n2
+
+#print(n,end='')
+#print('end')
+
+print(n,'end')
+
+
+if n1>n2:
+ print('n1 is greater')
+ print('n1 is greater')
+ print('n1 is greater')
+ print('n1 is greater')
+ print('n1 is greater')
+ print('n1 is greater')
+else:
+               print('n2 is greater')
+
+
+
+
+
+
+     
+
