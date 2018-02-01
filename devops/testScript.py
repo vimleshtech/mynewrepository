e2ad89ab3a51b17a9a1d749ddeedf8775a059032
vimleshtech <35063663+vimleshtech@users.noreply.github.com>
2018-02-01 01:11:36 -0600

    jenkins 1st feb

---
+++ devops/testScript.py
@@ -0,0 +1,59 @@
+# single line comment
+'''
+     multiple lien comment
+     multiple lien comment
+'''
+
+"""
+
+     multiple line comment
+     multiple line comment
+"""
+
+# declaration of variable
+a =1 #int # python does allocate memory by default
+print(type(a))
+
+a ='1' # str 
+print(type(a))
+
+a =True # boolean 
+print(type(a))
+
+a =44.3        #float 
+print(type(a))
+
+a =[2,3,4] # list []   / array 
+print(type(a))
+
+a ={1:'one',2:'two'} # dict {}{
+print(type(a))
+
+
+a =(1,2,3)# tuple ()
+print(type(a))
+
+#### input from console
+n1  = input('enter data ')
+n2  = input('enter data ')
+#print(type(n1))
+#print(type(n2))
+
+n = int(n1)+int(n2)
+print(n)
+
+### condition
+### python is identation based progaming language
+
+if n1>n2:
+     print('n1 is greater')
+else:
+     print('n2 is greater')
+     
+
+
+
+
+
+
+     
